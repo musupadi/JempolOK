83be48321c69f8d4345d8f9a4760a59aaf9db9c1
musupadi <33887565+musupadi@users.noreply.github.com>
2020-11-26 14:28:05 +0700

    Iwa Weebs

--- app/src/main/java/com/destinyapp/jempolok/Activity/SplashScreenActivity.java
+++ app/src/main/java/com/destinyapp/jempolok/Activity/SplashScreenActivity.java
@@ -2,11 +2,13 @@ package com.destinyapp.jempolok.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.destinyapp.jempolok.Model.Musupadi;
 import com.destinyapp.jempolok.R;
 import com.destinyapp.jempolok.Services;
@@ -14,11 +16,13 @@ import com.destinyapp.jempolok.SharedPreferance.DB_Helper;
 
 public class SplashScreenActivity extends AppCompatActivity {
     Musupadi musupadi;
+    LottieAnimationView lottie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         musupadi = new Musupadi();
         setContentView(R.layout.activity_splash_screen);
+        lottie = findViewById(R.id.lottie);
         final Handler handler = new Handler();
         final DB_Helper dbHelper = new DB_Helper(SplashScreenActivity.this);
         Cursor cursor = dbHelper.checkUser();
@@ -26,13 +30,29 @@ public class SplashScreenActivity extends AppCompatActivity {
             musupadi.Back(SplashScreenActivity.this);
             finish();
         }else{
-            handler.postDelayed(new Runnable() {
-                public void run() {
+            lottie.addAnimatorListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
                     Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
                     startActivity(intent);
                     finish();
                 }
-            }, 3000); //3000 L = 3 detik
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
         }
     }
 }
